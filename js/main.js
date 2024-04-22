151a9ee01c0dc1a3fc7ef52508d72be8bd9e7eaa
Ran GUO <74646749+Ran-GUO@users.noreply.github.com>
2024-04-22 09:50:02 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const key = 'ayoPJhNrPgf1eLBVDNY3';
+
+const attribution = new ol.control.Attribution({
+  collapsible: false,
+});
+
+const source = new ol.source.TileJSON({
+  url: `https://api.maptiler.com/maps/streets-v2/tiles.json?key=${key}`,
+  tileSize: 512,
+  crossOrigin: 'anonymous'
+});
+
+const map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: source
+    })
+  ],
+  controls: ol.control.defaults.defaults({attribution: false}).extend([attribution]),
+  target: 'map',
+  view: new ol.View({
+    constrainResolution: true,
+    center: ol.proj.fromLonLat([0, 0]),
+    zoom: 2
+  })
+});
+const layer = new ol.layer.Vector({
+
+});
+map.addLayer(layer);
+
+
+/*
+const layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    features: [
+      new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([12.550343, 55.665957])),
+      })
+    ]
+  }),
+  style: new ol.style.Style({
+    image: new ol.style.Icon({
+      anchor: [0.5, 1],
+      crossOrigin: 'anonymous',
+      src: 'image/marker-icon.png',
+    })
+  })
+});*/
