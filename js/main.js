ac20f509eb2d6319ec3053c7cac43ff32cb9f785
Ran GUO <74646749+Ran-GUO@users.noreply.github.com>
2024-04-22 12:16:39 +0200

    Modify folder name to images, correct marker src

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ const layer = new ol.layer.Vector({
     image: new ol.style.Icon({
       anchor: [0.5, 1],
       crossOrigin: 'anonymous',
-      src: '../image/marker-icon.png',
+      src: '../images/marker-icon.png',
     })
   })
 });
